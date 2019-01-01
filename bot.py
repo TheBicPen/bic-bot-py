21b8931b3f963f607f8ddbe46c02f41c5773f481
Bic <mashkal2000@gmail.com>
2018-12-31 20:10:55 -0800

    added commands. Added incompplete commands file. added modifyable command initialization string. added settings. added documentation to read_file. rad_file now creates the file if it doesn't exist.

--- bot.py
+++ bot.py
@@ -1,16 +1,38 @@
 # Work with Python 3.6
 import discord
 
-def get_token(file:str) -> str:
-    file_IO = open(file, "r")
-    token = file_IO.read().strip("\n")
-    file_IO.close()
-    return token
+def read_file(file:str) -> list:
+    """
+    Returns the contents of the file. If the file does not exist, creates the file
+    and returns an empty list.
+    """
+    try:
+        file_IO = open(file, "r")
+        contents = file_IO.read().splitlines()
+        file_IO.close()
+    except:
+        file_IO = open(file, "w")
+        file_IO.close()
+        contents = []
+    return contents
 
+def parse_message(msg):
+    if msg.startswith(command_str):
+        msg = msg[len(command_str):]
+    
+    command_list = ["hello", "myname", "isbot", "ping", 
+           " commit", "extrathicc","version", "callme"]
 
-TOKEN = get_token("token.txt")
+def initialize():
+    default_settings = {
+            command_str = "bb "
+            }
 
-client = discord.Client()
+    TOKEN = read_file("token.txt")[0]
+    settings = read_file("settings.txt")
+    client = discord.Client()
+
+initialize()
 
 @client.event
 async def on_message(message):
@@ -18,7 +40,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith(command_str):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
---
+++ commands.py
@@ -0,0 +1,18 @@
+def hello(message):
+    return 'Hello {0.author.mention}'.format(message)
+
+def ping(message):
+    return "pong!"
+
+def isbot(message):
+    return "yes"
+
+def commit(message):
+    
+
+def version(message):
+
+def callme(message):
+
+def myname(message):
+    get_name(message.author)
