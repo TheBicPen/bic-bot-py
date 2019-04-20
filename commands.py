73e9429644caa32d9469afb1061a3d1c7bcfaa1b
unknown <mashkal2000@gmail.com>
2019-04-20 17:51:44 -0400

    fixed tf_start and stop functions not working properly

--- commands.py
+++ commands.py
@@ -159,16 +159,18 @@ def start_tf():
     global classifications
     classifications = ic.get_classifications()
     if tf_sess is not None:
-        print("tensorFlow session already exists")
-        return tf_sess
+        print("TensorFlow session already exists")
+        return "TensorFlow session already exists"
     tf_sess = ic.main()
-    return tf_sess
+    return "started TensorFlow session"
 
 def stop_tf():
     global tf_sess
     global classifications
-    tf_sess.close()
+    tf_sess.close() # free resources used by the tf session
+    tf_sess = None # remove the reference to it
     classifications = None
+    print("ended TensorFlow session")
     return "ended TensorFlow session"
 
 async def image_category(message, tf_sess, classifications):
@@ -190,7 +192,7 @@ async def image_category(message, tf_sess, classifications):
         img = await response.read()
         print(type(img))
         result = ic.classify_image(img, tf_sess, classifications)
-    return "image is a {0}. {1:.2f} % confident".format(result[0], result[1])
+    return "image is a {0}. {1:.3f} % confident".format(result[0], result[1])
 
 #params: global_lists only
 def list_response(l:list):
