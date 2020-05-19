1206b540c5c904e1aea98177456f6f1d02ca14b3
bic <mashkal2000@gmail.com>
2020-05-19 10:27:23 -0700

    Fix imports and indents

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,8 @@
 # Work with Python 3.6
 import discord
-from . import message_parser as parser
-from . import helpers
-from . import consts
+import message_parser as parser
+import helpers
+import consts
 from sys import argv
 #import log
 
@@ -30,40 +30,40 @@ def main():
     client = discord.Client()
 
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_message(message):
+        # we do not want the bot to reply to itself
+        if message.author == client.user:
+            return
 
-    elif message.content.startswith("!debug"):
-        msg = 'Hello {0.author.mention}. The command string is {1}'.format(
-            message, settings["command_str"])
+        elif message.content.startswith("!debug"):
+            msg = 'Hello {0.author.mention}. The command string is {1}'.format(
+                message, settings["command_str"])
 
+        else:
+            msg = await message_parser.parse_message(message)
+
+        if not msg is None:
+            try:
+                if len(str(msg)) > 2000:
+                    helpers.log("message too long: " + str(len(str(msg))))
+                    await message.channel.send("message too long: " + str(len(str(msg))))
+                elif len(str(msg)) > 0:
+                    await message.channel.send(msg)
+            except:
+                helpers.log("Failed to process message of type " + str(type(msg)))
+
+
+    @client.event
+    async def on_ready():
+        helpers.log('Logged in as')
+        helpers.log(client.user.name)
+        helpers.log(client.user.id)
+        helpers.log('------')
+    if type(TOKEN) == type(""):
+        client.run(TOKEN)
     else:
-        msg = await message_parser.parse_message(message)
-
-    if not msg is None:
-        try:
-            if len(str(msg)) > 2000:
-                helpers.log("message too long: " + str(len(str(msg))))
-                await message.channel.send("message too long: " + str(len(str(msg))))
-            elif len(str(msg)) > 0:
-                await message.channel.send(msg)
-        except:
-            helpers.log("Failed to process message of type " + str(type(msg)))
-
-
-@client.event
-async def on_ready():
-    helpers.log('Logged in as')
-    helpers.log(client.user.name)
-    helpers.log(client.user.id)
-    helpers.log('------')
-if type(TOKEN) == type(""):
-    client.run(TOKEN)
-else:
-    helpers.log("Invalid token type: " + str(type(TOKEN)))
+        helpers.log("Invalid token type: " + str(type(TOKEN)))
 
 
 if __name__ == "__main__":
